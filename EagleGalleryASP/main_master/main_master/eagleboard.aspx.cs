f5e4439d8fe4380cd7c8716e5fdc64320d25cd90
hecodesfortheusers <jwjoines@eagles.usi.edu>
2019-09-18 23:58:42 -0500

    major tab update
    made each tab show its correct board. made the code cleaner(still needs comments)
    objectified rows for easier transition into database fetching

--- EagleGalleryASP/main_master/main_master/eagleboard.aspx.cs
+++ EagleGalleryASP/main_master/main_master/eagleboard.aspx.cs
@@ -11,46 +11,97 @@ namespace main_master
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            List<data_row> all_rows = new List<data_row>();
+            all_rows.Add(new data_row("poll_board", "are you happy?", DateTime.Now, "http:this"));
+            all_rows.Add(new data_row("gives_board", "want free happy?!?", DateTime.Now, "http:that"));
+            all_rows.Add(new data_row("gives_board", "free microwave", DateTime.Now, "http:that"));
+            all_rows.Add(new data_row("gives_board", "will test for free", DateTime.Now, "http:that"));
+            all_rows.Add(new data_row("gives_board", "smelly rug free", DateTime.Now, "http:that"));
+            all_rows.Add(new data_row("project_board", "lets make an app", DateTime.Now, "http:this"));
+            all_rows.Add(new data_row("project_board", "lets make a better app than him", DateTime.Now, "http:this"));
+            all_rows.Add(new data_row("poll_board", "do you like usi?", DateTime.Now, "http:this"));
+            all_rows.Add(new data_row("poll_board", "whats 5*5*5?", DateTime.Now, "http:this"));
+            all_rows.Add(new data_row("poll_board", "where the party at?", DateTime.Now, "http:this"));
+            all_rows.Add(new data_row("project_board", "#trashtag", DateTime.Now, "http:this"));
 
-            all_lit.Text = create_table_string("all_board");
-            gives_lit.Text = create_table_string("gives_board");
-            project_lit.Text = create_table_string("project_board");
-            poll_lit.Text = create_table_string("poll_board");
+
+            convert_rows_to_string_and_publish(ref all_rows);
+
+
+
+
 
 
         }
 
+        void convert_rows_to_string_and_publish(ref List<data_row> data_rows) {
 
-        string create_row(string link, string title, string board, DateTime date) {
+            string table_string_all = @"<table class=""table"" id= ""all_board""> <thead class=""thead-dark""> <tr> <th scope=""col"">Board</th> <th scope=""col"">Title</th> <th scope=""col"">Date</th></tr> </thead> <tbody>";
+            string table_string_gives = @"<table class=""table"" id= ""gives_board""> <thead class=""thead-dark""> <tr> <th scope=""col"">Board</th> <th scope=""col"">Title</th> <th scope=""col"">Date</th></tr> </thead> <tbody>";
+            string table_string_project = @"<table class=""table"" id= ""project_board""> <thead class=""thead-dark""> <tr> <th scope=""col"">Board</th> <th scope=""col"">Title</th> <th scope=""col"">Date</th></tr> </thead> <tbody>";
+            string table_string_poll = @"<table class=""table"" id= ""poll_board""> <thead class=""thead-dark""> <tr> <th scope=""col"">Board</th> <th scope=""col"">Title</th> <th scope=""col"">Date</th></tr> </thead> <tbody>";
 
-
-
-            string row_string = @"<tr> <th scope=""row"">" + board + "</th> <td>" + title + "</td> <td>" + Convert.ToString(date) + "</td> </tr>";
-            //creates a row of a bootstrap table in html
-
-            return row_string;
-
-
-        }
-
-        string create_table_string(string board) {
-            string title;
-            string table_string = @"<table class=""table"" id= """ + board + @"""> <thead class=""thead-dark""> <tr> <th scope=""col"">Board</th> <th scope=""col"">Title</th> <th scope=""col"">Date</th></tr> </thead> <tbody>";
-           
-            for (int i = 0; i < 500; i++)
-           {
-                title = "this is post " + Convert.ToString(i);
-               table_string = table_string + create_row("nothing", title, "Poll", DateTime.Now );
-                
+            foreach (data_row r in data_rows) {
+                table_string_all = table_string_all + r.get_row_string();
+                if (r.get_board_id().CompareTo("gives_board") == 0) { table_string_gives = table_string_gives + r.get_row_string(); }
+                else if (r.get_board_id().CompareTo("project_board") == 0) { table_string_project = table_string_project + r.get_row_string(); }
+                else if (r.get_board_id().CompareTo("poll_board") == 0) { table_string_poll = table_string_poll + r.get_row_string(); }
             }
-            //combines rows of html table and spits out a whole table!
 
-            table_string = table_string +  "</tbody></table>";
-            return table_string;
+            table_string_all = table_string_all + "</tbody></table>";
+            table_string_gives = table_string_gives + "</tbody></table>";
+            table_string_project = table_string_project + "</tbody></table>";
+            table_string_poll = table_string_poll + "</tbody></table>";
+
+            all_lit.Text = table_string_all;
+            gives_lit.Text = table_string_gives;
+            project_lit.Text = table_string_project;
+            poll_lit.Text = table_string_poll;
 
         }
 
 
+        void build_data() {
+
+            //placeholder for reading database
+        }
+
+    }
+
+     class data_row {
+        const string poll_title = "Poll";
+        const string project_title = "Project";
+        const string gives_title = "Gives";
+        string board_id;
+        string board;
+        string title;
+        DateTime date;
+        string row_string = "";
+        string link;
+        
+
+
+
+        public data_row(string in_board_id, string in_title, DateTime in_date, string in_link) {
+            title = in_title;
+            date = in_date;
+            link = in_link;
+            board_id = in_board_id;
+            if (board_id.CompareTo("gives_board") == 0) { board = gives_title; }
+            else if (board_id.CompareTo("project_board") == 0) { board = project_title; }
+            else if (board_id.CompareTo("poll_board") == 0) { board = poll_title; }
+        }
+
+        public string get_row_string()
+        {
+            row_string = @"<tr> <th scope=""row"">" + board + "</th> <td>" + title + "</td> <td>" + Convert.ToString(date) + "</td> </tr>";
+            //creates a row of a bootstrap table in html
+            return row_string;
+        }
+
+        public string get_board_id() {
+            return board_id;
+        }
 
 
 
